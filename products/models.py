b493d2900d7d3242b9b036d995d317cbfa9d0734
MaksimochkinBogdan <116176291+MaksimochkinBogdan@users.noreply.github.com>
2023-03-02 21:36:55 +0300

    lesson5 (fixtures)

--- products/models.py
+++ products/models.py
@@ -15,7 +15,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
-    image = models.ImageField(upload_to='products_images', null=True, blank=True)
+    image = models.ImageField(upload_to='products_images')
     category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
 
     def __str__(self):
