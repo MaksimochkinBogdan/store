9486de74bd1ef2ee5444272046792f823880f241
MaksimochkinBogdan <116176291+MaksimochkinBogdan@users.noreply.github.com>
2023-03-02 20:26:40 +0300

    lesson5 (views QuerySet)

--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from products.models import Product, ProductCategory
+
 # Create your views here.
 
 def index(request):
@@ -11,5 +13,7 @@ def index(request):
 def products(request):
     context = {
 		'title':'Store - Каталог',
+		'products': Product.objects.all(),
+		'categories': ProductCategory.objects.all(),
 	}
     return render(request, 'products/products.html', context)
