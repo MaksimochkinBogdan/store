cce704576d101f1343aad3434bc08635cb15e09a
MaksimochkinBogdan <116176291+MaksimochkinBogdan@users.noreply.github.com>
2023-03-02 20:05:40 +0300

    lesson5 (admin)

--- products/models.py
+++ products/models.py
@@ -6,11 +6,17 @@ class ProductCategory(models.Model):
     name = models.CharField(max_length=128)
     description = models.TextField(null=True, blank=True)
 
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     name = models.CharField(max_length=256)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
-    image = models.ImageField(upload_to='products_images')
+    image = models.ImageField(upload_to='products_images', null=True, blank=True)
     category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f'Продукт: {self.name} | Категория: {self.category.name}'
